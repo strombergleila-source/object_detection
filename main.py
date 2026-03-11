6b8110c174ebd8cf60f78439af9f11aef18b1359
strombergleila-source <strombergleila@gmail.com>
2026-03-11 15:58:53 +0100

    final changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import cv2
 import matplotlib.pyplot as plt
@@ -6,29 +5,22 @@ import numpy as np
 from ultralytics import YOLO
 
 
-""" 
 # Create and train the model
+# It took very long time so we used Colab instead.
 model = YOLO("yolov8n.pt")
 model.train(
     data="dataset/data.yaml",
     epochs=50,
     imgsz=640
 ) 
-"""
+
 # Run the model on the test images
-""" model = YOLO("best.pt")
+model = YOLO("best.pt")
 model.predict(
     source="dataset/test/images",
     save=True,
     conf=0.25 
-)"""
+)
 
-#model.val(data="dataset/data.yaml")
-""" model = YOLO("best.pt")
-model.val(data="dataset/data.yaml", plots=True) """
-
-# test the model with the webcam
-# model = YOLO("best.pt")
-# model.predict(source=0, show=True)
-
-
+model = YOLO("best.pt")
+model.val(data="dataset/data.yaml", plots=True)
